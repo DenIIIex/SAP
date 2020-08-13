e3ca822605c604bb419bb8eb7508d506301eab18
Denis <ivankov@monitorsoft.ru>
2020-08-13 10:20:56 +0300

    Refactor code

--- Script/EndLevel.gd
+++ Script/EndLevel.gd
@@ -1,13 +1,11 @@
 extends ColorRect
 onready var Global = get_tree().get_root().find_node("Global", true, false)
 
-
 func _ready():
 	hide()
 
-
 func _on_RestartBut_pressed():
-	Global.startLevel(Global.getLevel(), Global.getLevelStartPos(), true)
+	Global.startLevel(Global.getLevel(),Global.getLevelStartPos(),true)
 	Global.setCanMove(true)
 	hide()
 
@@ -15,6 +13,7 @@ func _on_RestartBut_pressed():
 func _on_NextBut_pressed():
 	Global.loadNextLevel()
 	hide()
+	
 
 
 func _on_ExitToMainMenu_pressed():
